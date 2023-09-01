8d40adab3f7e59c659da9e75720bbd62b9c3ef5e
Guillermo Aviles <gjaviles7@gmail.com>
2023-08-31 22:48:04 -0400

    testing api calls

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 function HomePage() {
   const [showCheckingAccounts, setShowCheckingAccounts] = useState(false);
   const [showSavingsAccounts, setShowSavingsAccounts] = useState(false);
   const [showInvestmentAccounts, setShowInvestmentAccounts] = useState(false);
+
+  const [checkingAccounts, setCheckingAcounts] = useState([]);
+  const [savingsAccounts, setSavingsAccounts] = useState([]);
+  const [investmentAccounts, setInvestmentAccounts] = useState([]);
+
+  const apiURL = "http://localhost:8080/api/accounts/checking";
+
+  // const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJndWlsbGUiLCJyb2xlcyI6WyJST0xFX1VTRVIiXSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2FwaS9sb2dpbiIsImV4cCI6MTY5MzUzNjMxM30.fEyUCD9QgV0v24v5NEdQOJhY7Ned481jssDxovtOoFg";
+
+  // const config = {
+  //   headers: { Authorization: `Bearer ${token}` },
+  // };
+
+  // const bodyParameters = {
+  //   key: "value",
+  // };
+
+  useEffect(() => {
+    console.log("useEffect - Initial render (Mounting)");
+    axios.get(apiURL).then((response) => {
+      setCheckingAcounts(response.data);
+      setFetching(false);
+    });
+  }, []);
 
   const handleCheckingAccountsClick = () => {
     setShowCheckingAccounts(!showCheckingAccounts);
@@ -18,6 +43,8 @@ function HomePage() {
     setShowInvestmentAccounts(!showInvestmentAccounts);
   };
 
+  console.log("checkingAccounts", checkingAccounts);
+
   return (
     <div className="home-container">
       <h1 className="welcome">Welcome to your account, Guillermo.</h1>
@@ -26,16 +53,16 @@ function HomePage() {
       </div>
       {showCheckingAccounts && (
         <>
-          <div className="link-card">
+          <div className="account-card">
             <Link className="link" to={"/accounts/checking/test"}>
               <h2>Checking Account 1</h2>
               <p>Available Balance: $100</p>
             </Link>
           </div>
-          <div className="link-card">
+          <div className="account-card">
             <Link className="link" to={"/accounts/checking/test"}>
               <h2>Checking Account 2</h2>
-              <p>Available Balance: $100</p>
+              <p>Available Balance: $85.50</p>
             </Link>
           </div>
         </>
@@ -45,10 +72,10 @@ function HomePage() {
       </div>
       {showSavingsAccounts && (
         <>
-          <div className="link-card">
+          <div className="account-card">
             <Link className="link" to={"/accounts/savings/test"}>
               <h2>Savings Account 1</h2>
-              <p>Available Balance: $100</p>
+              <p>Available Balance: $150</p>
             </Link>
           </div>
         </>
@@ -58,13 +85,13 @@ function HomePage() {
       </div>
       {showInvestmentAccounts && (
         <>
-          <div className="link-card">
+          <div className="account-card">
             <Link className="link" to={"/accounts/investment/test"}>
               <h2>Investment Account 1</h2>
               <p>Total Equity: $24,502</p>
             </Link>
           </div>
-          <div className="link-card">
+          <div className="account-card">
             <Link className="link" to={"/accounts/investment/test"}>
               <h2>Investment Account 2</h2>
               <p>Total Equity: $2,212</p>
